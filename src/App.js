bf20bb41e1e892f74f5920b9cfa844e477678774
Admin <arnodejong@outlook.com>
2018-04-03 12:01:47 +0200

    feat: adds browser history and Home

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Router, Route } from 'react-router';
+import createHistory from 'history/createBrowserHistory';
 import { Provider } from 'react-redux';
 import configureStore from './state/store';
 import './App.css';
+import Home from './components/Home';
 import DevTools from './containers/DevTools';
 
 const store = configureStore();
+const history = createHistory();
 
 const App = () => (
   <Provider store={store}>
     <div>
       <DevTools />
+      <Router history={history}>
+        <Route path="/" component={Home} />
+      </Router>
     </div>
   </Provider>
 );
+
 
 export default App;
