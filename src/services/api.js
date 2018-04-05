27bda3b75ea341b789624b8b09ee2626ee89764f
sven-zo <svenzo@outlook.com>
2018-04-05 11:19:55 +0200

    feat: adds api service

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+
+const { API_URL } = process.env;
+
+export default async function fetchPackage(id) {
+  try {
+    const response = await fetch(API_URL);
+    const parcel = await response.json();
+    return parcel;
+  } catch (error) {
+    return new Error('fetch failed');
+  }
+}
