f567e96684198fe1ebf3a3c616c06c52bc6ffce7
Admin <arnodejong@outlook.com>
2018-05-29 16:58:02 +0200

    feat: adds trackingcode

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,12 +4,14 @@ import Menu from '../Menu';
 import Time from '../Time';
 import Arrive from '../Arrive';
 import CardCarousel from '../CardCarousel';
+import TrackingCode from '../TrackingCode';
 
 const Home = () => (
   <div className="home">
     <Menu skeleton={false} />
     <CardCarousel skeleton={false} />
     <Arrive signature />
+    <TrackingCode></TrackingCode>
   </div>
 );
 
---
+++ src/components/TrackingCode/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+const TrackingCode = () => (
+  <div className="tracking-code">
+    <h3>DT123456789102</h3>
+    <p>Tracking code</p>
+  </div>
+);
+
+export default TrackingCode;
