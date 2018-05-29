5b168b2ce86e440c89056aba58dcb26de1fad706
sven-zo <svenzo@outlook.com>
2018-05-29 14:54:06 +0200

    feat: adds Arrive component

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './style.css';
 import Menu from '../Menu';
 import Time from '../Time';
+import Arrive from '../Arrive';
 
 const Home = () => (
   <div className="home">
     <Menu skeleton={false} />
     <Time skeleton={false} />
+    <Arrive signature />
   </div>
 );
 
