3cfd5b15129c41e3ee8d70d368efcc90bfdcd0d0
Admin <arnodejong@outlook.com>
2018-05-31 13:47:33 +0200

    feat: adds package information

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './style.css';
 import Menu from '../Menu';
-import Time from '../Time';
 import Arrive from '../Arrive';
 import CardCarousel from '../CardCarousel';
 import TrackingCode from '../TrackingCode';
+import PackageInformation from '../PackageInformation';
 
 const Home = () => (
   <div className="home">
     <Menu skeleton={false} />
     <CardCarousel skeleton={false} />
     <Arrive signature />
-    <TrackingCode></TrackingCode>
+    <TrackingCode />
+    <PackageInformation />
   </div>
 );
 
---
+++ src/components/PackageInformation/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const TrackingCode = () => (
+  <div className="package-information">
+    <h3>Albert Heijn<br />
+        4 kg<br />
+        10 x 50 x 30 cm<br />
+    </h3>
+  </div>
+);
+
+export default TrackingCode;
