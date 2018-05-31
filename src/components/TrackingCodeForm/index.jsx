0459e9144c16cc8d493d1854f329efb5a1608f70
sven-zo <svenzo@outlook.com>
2018-05-31 16:45:10 +0200

    feat: adds milkyGlassBox styles to TrackingCodeForm

--- src/components/TrackingCodeForm/index.jsx
+++ src/components/TrackingCodeForm/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './style.css';
 import Menu from '../Menu';
+import packageIcon from './package-icon-anim.svg';
+import nextButton from './next-button.svg';
 
 const TrackingCodeForm = () => (
-  <div className="home">
+  <div className="tracking-code-form">
     <Menu skeleton={false} />
+    <img id="packageIcon" src={packageIcon} alt="" />
+    <div className="milkyGlassBox">
+      <h2>Please fill in your tracking code to continue.</h2>
+      <input type="text" />
+      <p>Tracking code</p>
+      <img src={nextButton} alt="next" />
+    </div>
   </div>
 );
 
